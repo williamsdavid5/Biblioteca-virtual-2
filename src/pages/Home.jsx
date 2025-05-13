4725603d6ea644d73b0a6d382e715e75a951a38f
David Duarte (Williams) <davidduart04@gmail.com>
2025-05-13 14:54:49 -0300

    tela inicial concluida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import './Home.css'
+import livrosImg from '../assets/images/livrosPrateleira.png'
+import { useNavigate } from 'react-router-dom'
+
 export default function Home() {
+    const navigate = useNavigate()
+
     return (
-        <h1>
-            home
-        </h1>
+        <div className="container">
+            <main>
+                <p>Bem vindo(a) a nossa</p>
+                <h1>Biblioteca Virtual</h1>
+                <p>Você deveria ter feito seu login antes dessa tela, mas o nosso dev não programou essa função :(</p>
+                <div id='botoesFuncoes'>
+                    <button onClick={() => navigate('/emprestar')}>Emprestar</button>
+                    <button onClick={() => navigate('/gerenciar')}>Gerenciar</button>
+                </div>
+            </main>
+            <div id='imagem'>
+                <img src={livrosImg} alt="" className='animacao' />
+            </div>
+        </div>
     )
 }
