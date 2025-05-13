b6f9d39cb789cfc6705d56a949fde8336291f4c1
David Duarte (Williams) <davidduart04@gmail.com>
2025-05-13 19:43:59 -0300

    conexão bem sucedida com o banco de dados onlin

--- src/pages/Gerenciar.jsx
+++ src/pages/Gerenciar.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+
 export default function Gerenciar() {
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        async function fetchLivros() {
+            try {
+                const response = await api.get('/livros');
+                setLivros(response.data);
+
+                console.log(livros);
+
+            } catch (error) {
+                console.log('Erro ao buscar livros: ', error);
+            }
+        }
+
+        fetchLivros();
+
+    })
+
     return <>Gerenciar</>
 }
